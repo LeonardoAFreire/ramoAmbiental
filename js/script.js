85afd0e42c1651ac8c35a4dde2ff1cc08c3e75e9
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-04-19 19:15:24 -0300

    funcionamento

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initBtnMobile from './modules/btn-mobile.js';
 import initAnimaScroll from './modules/anima-scroll.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
 
 initBtnMobile();
 initAnimaScroll();
 initAnimaNumeros();
+initFuncionamento();
