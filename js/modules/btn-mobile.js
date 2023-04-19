768c80af072f3129752b59d81bebc17854ddc321
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-04-18 23:59:22 -0300

    menu-mobile

--- js/modules/btn-mobile.js
+++ js/modules/btn-mobile.js
@@ -1,22 +1,24 @@
-export default function initBtnMobile() {}
+export default function initBtnMobile() {
+  const btnMobile = document.getElementById('btn-mobile');
 
-const btnMobile = document.getElementById('btn-mobile');
+  if (btnMobile) {
+    function handleClick(event) {
+      // TouchStart tem o evento de click no final que se nao for tirado vai bugar o codigo
+      if (event.type === 'touchstart') {
+        event.preventDefault();
+      }
+      const nav = document.getElementById('nav');
+      nav.classList.toggle('active');
+      const active = nav.classList.contains('active');
+      event.currentTarget.setAttribute('aria-expanded', active);
+      if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+      } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+      }
+    }
 
-function handleClick(event) {
-  // TouchStart tem o evento de click no final que se nao for tirado vai bugar o codigo
-  if (event.type === 'touchstart') {
-    event.preventDefault();
-  }
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    btnMobile.addEventListener('click', handleClick);
+    btnMobile.addEventListener('touchstart', handleClick);
   }
 }
-
-btnMobile.addEventListener('click', handleClick);
-btnMobile.addEventListener('touchstart', handleClick);
