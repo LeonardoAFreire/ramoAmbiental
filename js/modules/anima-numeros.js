e8d76360fb20eb26552a4fe2e7e105018f6bb485
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-04-19 18:42:12 -0300

    animacao numeros

---
+++ js/modules/anima-numeros.js
@@ -0,0 +1,43 @@
+export default function initAnimaNumeros() {
+  const numeros = document.querySelectorAll('[data-number]');
+  if (numeros) {
+    limpaNumeros(numeros);
+
+    function animaNumeros() {
+      numeros.forEach((numero) => {
+        const total = +numero.dataset.number;
+        const incremento = Math.ceil(total / 100);
+        let start = 0;
+
+        const timer = setInterval(() => {
+          start += incremento;
+          numero.innerText = start;
+
+          if (start > total) {
+            numero.innerText = total;
+            clearInterval(timer);
+          }
+        }, 50 * Math.random());
+      });
+    }
+
+    function handleMutation(mutation) {
+      const mudou = mutation[0].target.classList.contains('ativo');
+      if (mudou) {
+        observer.disconnect();
+        animaNumeros();
+      }
+    }
+
+    const observerTarget = document.querySelector('.numero-contador');
+    const observer = new MutationObserver(handleMutation);
+
+    observer.observe(observerTarget, { attributes: true });
+  }
+}
+
+function limpaNumeros(numeros) {
+  numeros.forEach((numero) => {
+    numero.innerText = 0;
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 import initBtnMobile from './modules/btn-mobile.js';
+import initAnimaScroll from './modules/anima-scroll.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initBtnMobile();
+initAnimaScroll();
+initAnimaNumeros();
